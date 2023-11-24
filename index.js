14e6689f1fae435062a9b68b9703c683ce5a3581
jardsoNpeixoto123 <jardson.peixoto05@aluno.ifce.edu.br>
2023-11-24 14:58:43 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+const port = 3000;
+const produtos = [];
+
+
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+
+app.get('/meusprodutos', (req, res) => {
+    console.log(produtos);
+    res.render('meusprodutos', { produtos });
+});
+
+app.post('/cadastrar', (req, res) => {
+    
+    const { nomeProduto, nomeFornecedor, dataCompra, valorCompra, valorVenda, categoria } = req.body;
+
+    
+    const novoProduto = {
+        nomeProduto,
+        nomeFornecedor,
+        dataCompra,
+        valorCompra,
+        valorVenda,
+        categoria,
+    };
+    
+    res.redirect('/meusprodutos');
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
+
